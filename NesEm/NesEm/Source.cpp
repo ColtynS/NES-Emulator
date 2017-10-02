e95684cf79d268d200aabddaa3163fbb20099b10
ColtynS <32465079+ColtynS@users.noreply.github.com>
2017-10-02 16:06:50 -0400

    Adding of Flags and RAM

--- NesEm/NesEm/Source.cpp
+++ NesEm/NesEm/Source.cpp
@@ -1,6 +1,32 @@
-#include <iostream>	
+#include <iostream>
+#include <SFML\Graphics.hpp>
+#include "cpu.h"
+
+const int height = 224;
+const int width = 256;
+const double CPU_SPEED = 1789773;
+const double PPU_SPEED = 5369318;
+
 int main()
 {
 	std::cout << "I'm an emulator" << std::endl;
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
 	return 0;
 }
--- NesEm/NesEm/cpu.h
+++ NesEm/NesEm/cpu.h
@@ -1,9 +1,32 @@
 #pragma once
 class cpu {
 private:
+	//CPU
+	unsigned int PC;//program counter
+	unsigned short SP;//stack pointer/position
+	unsigned char X, Y; //x and y registers temp storage in cpu
+	unsigned char A; //accumilator stores math output
+	bool C;//carryFlag;//true if carrys over to 0 from 255
+	bool Z;// zeroFlag; //true if math == 0
+	bool I;// interruptDisable;//true when it doesnt accept IRQ which interupts CPU
+	bool D;//decimalMode;//NES cpu doesnt use
+	bool B;//breakCommand;//causes an IRQ to disrupt the CPU
+	bool V;//overFlowFlag;//APPENDIX A
+	bool N;//negativeFlag;//true if sign bit is 1 
+	/*	Flag storage
+		7 6 5 4 3 2 1 0
+		N V   B D I Z C
+
+	*/
+	
+	//RAMs
+	unsigned char RAM[65536];// Assuming 64kb
+	unsigned char VRAM[65536];// Assuming 64kb
+
+
 public: 
 	cpu() 
 	{
-
+		
 	}
 };
